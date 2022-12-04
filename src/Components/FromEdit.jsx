dbf9cd9effaee2bea66dc5fcaf44cf2812a004f6
Zuko <jose.apep570@gmail.com>
2022-12-04 09:32:27 -0600

    [ADD] Get logo in From edit

--- src/Components/FromEdit.jsx
+++ src/Components/FromEdit.jsx
@@ -93,6 +93,33 @@ function FromEdit({
       .catch((error) => {
         console.error("Error:", error);
       });
+
+      
+    /* fetch(`http://localhost:8080/image/getLogo/${idRestaurant}`, {
+      method: "GET",
+      headers: {
+        Accept: "aplication/json",
+        "Content-Type": "Aplication/json",
+      },
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((respuesta) => setZone(respuesta.data))
+      .catch((error) => {
+        console.error("Error:", error);
+      }); */
+
+
+
+      
       };
 
   const changeHandler = (e) => {
